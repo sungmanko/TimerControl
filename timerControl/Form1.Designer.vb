76271264403b1d500b1a664ad26de8bf415e2230
sungman ko <33561581+sungmanko@users.noreply.github.com>
2018-12-18 17:52:21 +0900

    timerControl
    Calculate and display the remaining time until d-day

---
+++ timerControl/Form1.Designer.vb
@@ -0,0 +1,92 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.lblTimer = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.lblDay = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Timer1
+        '
+        '
+        'lblTimer
+        '
+        Me.lblTimer.Location = New System.Drawing.Point(12, 200)
+        Me.lblTimer.Name = "lblTimer"
+        Me.lblTimer.Size = New System.Drawing.Size(311, 23)
+        Me.lblTimer.TabIndex = 0
+        Me.lblTimer.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label1
+        '
+        Me.Label1.Font = New System.Drawing.Font("MS UI Gothic", 13.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Label1.Location = New System.Drawing.Point(50, 21)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(226, 23)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "대학 수학 능력 시험"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label2
+        '
+        Me.Label2.Location = New System.Drawing.Point(140, 44)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(111, 23)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = " 일 남았습니다."
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'lblDay
+        '
+        Me.lblDay.Font = New System.Drawing.Font("MS UI Gothic", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.lblDay.Location = New System.Drawing.Point(90, 44)
+        Me.lblDay.Name = "lblDay"
+        Me.lblDay.Size = New System.Drawing.Size(52, 23)
+        Me.lblDay.TabIndex = 3
+        Me.lblDay.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(335, 231)
+        Me.Controls.Add(Me.lblDay)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.lblTimer)
+        Me.Name = "Form1"
+        Me.Text = "학생 관리 프로그램"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents lblTimer As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents lblDay As Label
+End Class
---
+++ timerControl/Form1.vb
@@ -0,0 +1,34 @@
+﻿Public Class Form1
+
+    Dim finalDay As String = "2018/01/11 09:00:00"
+    Dim startTime As String = Date.Now.ToString()
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Timer1.Interval = 1000
+        Timer1.Start()
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Dim nowTime As String = Date.Now.ToString()
+
+        Dim total, duration1, duration2 As TimeSpan
+        duration1 = DateTime.Parse(nowTime).Subtract(DateTime.Parse(startTime))
+        total = duration1.Add(duration2)
+
+        lblTimer.Text = total.ToString()
+
+        If lblDay.Text = String.Empty Then
+            Dim result As String = DateDiff(DateInterval.Day, DateTime.Now(), DateTime.Parse(finalDay)).ToString()
+            If "0".Equals(result) Then
+                lblDay.Text = "d-Day"
+                Label2.Text = String.Empty
+            Else
+                lblDay.Text = DateDiff(DateInterval.Day, DateTime.Now(), DateTime.Parse(finalDay)).ToString()
+            End If
+        End If
+
+    End Sub
+
+End Class
